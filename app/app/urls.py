021fe49a4ea34e158fc8eca8fe6741748e9b355e
Onofre <oabenjumev@gmail.com>
2022-10-02 16:33:37 -0500

    Implemented listing and detail for recipe API

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api_docs',
-        ),
+    ),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
